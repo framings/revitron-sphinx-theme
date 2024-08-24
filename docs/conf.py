4f3a583845ee7ad3f0969d36fa85d38483300c03
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-08-24 17:51:09 +0100

    Add docs/conf.py

---
+++ docs/conf.py
@@ -0,0 +1,146 @@
+"""
+-*- coding: utf-8 -*-
+"""
+import sys
+import os
+import re
+import revitron_sphinx_theme
+import sphinx.locale
+import datetime
+
+
+'''
+If we are building locally, or the build on Read the Docs looks like a PR
+build, prefer to use the version of the theme in this repo, not the installed
+version of the theme.
+'''
+def is_development_build():
+    # PR builds have an interger version
+    re_version = re.compile(r'^[\d]+$')
+    if 'READTHEDOCS' in os.environ:
+        version = os.environ.get('READTHEDOCS_VERSION', '')
+        if re_version.match(version):
+            return True
+        return False
+    return True
+
+if is_development_build():
+    sys.path.insert(0, os.path.abspath('..'))
+sys.path.append(os.path.abspath('./demo/'))
+
+
+'''
+Later
+    version = ''
+    release = ''
+    author = ''
+    copyright = '<a href="https://github.com/"></a>'
+'''
+project = u'Revitron'
+slug = re.sub(r'\W+', '-', project.lower())
+language = 'en'
+author = 'public'
+copyright = '{}'.format(datetime.datetime.now().year)
+
+extensions = [
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.mathjax',
+    'sphinx.ext.viewcode',
+    'sphinxcontrib.httpdomain',
+    'revitron_sphinx_theme',
+    'autodocsumm'
+]
+
+autodoc_default_options = {
+    'autosummary': True 
+}
+
+templates_path = ['_templates']
+source_suffix = '.rst'
+exclude_patterns = []
+locale_dirs = ['locale/']
+gettext_compact = False
+
+master_doc = 'index'
+suppress_warnings = ['image.nonlocal_uri']
+
+intersphinx_mapping = {
+    'rtd': ('https://docs.readthedocs.io/en/latest/', None),
+    'sphinx': ('http://www.sphinx-doc.org/en/stable/', None),
+}
+
+html_theme = 'revitron_sphinx_theme'
+
+html_theme_options = {
+    'navigation_depth': 5,
+    'logo_mobile': 'revitron-ui-mobile.svg',
+    'github_url': 'https://github.com/revitron/revitron',
+	'color_scheme': 'dark'
+}
+
+html_context = {
+	'landing_page': {
+		'menu': [
+			{'title': 'Docs', 'url': 'installing.html'},
+			{'title': 'GitHub', 'url': 'https://github.com'}
+		]
+	} 
+}
+
+if not 'READTHEDOCS' in os.environ:
+    html_static_path = ['_static/']
+    html_js_files = ['debug.js']
+
+    # Add fake versions for local QA of the menu
+    html_context['test_versions'] = list(map(
+        lambda x: str(x / 10),
+        range(1, 100)
+    ))
+
+html_logo = 'demo/static/revitron.svg'
+html_title = 'Revitron'
+html_show_sourcelink = True
+htmlhelp_basename = slug
+
+latex_documents = [
+  ('index', '{0}.tex'.format(slug), project, author, 'manual'),
+]
+
+man_pages = [
+    ('index', slug, project, [author], 1)
+]
+
+texinfo_documents = [
+  ('index', slug, project, author, slug, project, 'Miscellaneous'),
+]
+
+
+'''
+Extensions to theme docs
+'''
+def setup(app):
+    import sphinx.domains.python as sdp
+    import sphinx.util.docfields as sud
+
+    app.add_object_type(
+        'confval',
+        'confval',
+        objname='configuration value',
+        indextemplate='pair: %s; configuration value',
+        doc_field_types=[
+            sdp.PyField(
+                'type',
+                label=_('Type'),
+                has_arg=False,
+                names=('type',),
+                bodyrolename='class'
+            ),
+            sud.Field(
+                'default',
+                label=_('Default'),
+                has_arg=False,
+                names=('default',),
+            ),
+        ]
+    )
